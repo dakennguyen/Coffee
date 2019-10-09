0830ef512038db23f1da7804c5d302c6e7d8cd0e
Khoa Nguyen <ngdoan.anhkhoa@gmail.com>
2019-10-09 11:47:09 +0700

    initial commit

---
+++ ClientApp/src/components/ExternalApi.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useAuth0 } from "../react-auth0-wrapper";
+
+const ExternalApi = () => {
+    const [showResult, setShowResult] = useState(false);
+    const [apiMessage, setApiMessage] = useState("");
+    const {
+        isAuthenticated,
+        logout,
+        loginWithRedirect,
+        getTokenSilently
+    } = useAuth0();
+    const logoutWithRedirect = () => {
+        localStorage.removeItem('token');
+        logout({
+            returnTo: window.location.origin
+        });
+    }
+
+    const callApi = async () => {
+        try {
+            var token = localStorage.getItem('token');
+            const response = await fetch(
+                "https://localhost:5000/api/SampleData/CallApi",
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+
+            const responseData = await response.json();
+
+            setShowResult(true);
+            setApiMessage(responseData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <>
+            <h1>External API</h1>
+            {!localStorage.getItem('token') && (
+                <button onClick={() => loginWithRedirect({})}>Login</button>
+            )}
+            {localStorage.getItem('token') && (
+                <button onClick={() => logoutWithRedirect()}>Logout</button>
+            )}
+            <button onClick={callApi}>Ping API</button>
+            {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
+        </>
+    );
+};
+
+export default ExternalApi;
---
+++ Controllers/SampleDataController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleDataController : ControllerBase
+    {
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public Dictionary<int, string> CallApi()
+        {
+            var rng = new Random();
+            return new Dictionary<int, string>
+            {
+                {1, "Called from Coffee server " + rng.Next(0, 100)}
+            };
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IEnumerable<WeatherForecast> WeatherForecasts()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+        }
+
+        public class WeatherForecast
+        {
+            public string DateFormatted { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+
+            public int TemperatureF
+            {
+                get
+                {
+                    return 32 + (int)(TemperatureC / 0.5556);
+                }
+            }
+        }
+    }
+}
