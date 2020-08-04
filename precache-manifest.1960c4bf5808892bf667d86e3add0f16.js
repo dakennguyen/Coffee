28f5b232608585f1672237a600d2078dcb585b87
Khoa Nguyen <ngdoan.anhkhoa@gmail.com>
2020-08-04 16:05:18 +0700

    Updates

---
+++ precache-manifest.1960c4bf5808892bf667d86e3add0f16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5745045e64c817ee764a721528eedb10",
+    "url": "/Coffee/index.html"
+  },
+  {
+    "revision": "aa89025b13ed135b2181",
+    "url": "/Coffee/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "aa89025b13ed135b2181",
+    "url": "/Coffee/static/js/2.64b934df.chunk.js"
+  },
+  {
+    "revision": "24b326dc74d6b6298922",
+    "url": "/Coffee/static/js/main.db9dc691.chunk.js"
+  },
+  {
+    "revision": "565623525245192e3853",
+    "url": "/Coffee/static/js/runtime-main.6c45de2a.js"
+  }
+]);
